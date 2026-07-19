b1eed17651a1f8c091d949c18e1cc4878f05dcdc
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 14:33:33 -0700

    Add ModeSetting class for cycling through modes

---
+++ src/main/java/com/plebys/settings/ModeSetting.java
@@ -0,0 +1,23 @@
+package com.plebys.settings;
+
+public class ModeSetting extends Setting {
+    private String value;
+    private final String[] modes;
+
+    public ModeSetting(String name, String... modes) {
+        super(name);
+        this.modes = modes;
+        this.value = modes[0];
+    }
+
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
+    public String[] getModes() { return modes; }
+    public void cycle() {
+        int index = 0;
+        for (int i = 0; i < modes.length; i++) {
+            if (modes[i].equals(value)) index = i;
+        }
+        value = modes[(index + 1) % modes.length];
+    }
+}

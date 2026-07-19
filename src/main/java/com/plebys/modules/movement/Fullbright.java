2624e4a8166ecc14f8f3d1a2b1be6c97cf6aa25d
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 14:49:37 -0700

    Refactor Fullbright module settings and methods

--- src/main/java/com/plebys/modules/movement/Fullbright.java
+++ src/main/java/com/plebys/modules/movement/Fullbright.java
@@ -5,32 +5,26 @@ import com.plebys.modules.Module;
 import com.plebys.settings.*;
 
 public class Fullbright extends Module {
-    public final SliderSetting gamma = addSlider("Gamma", 1.0, 20000.0, 10000.0);
-    public final BooleanSetting nightVision = addBoolean("Night Vision Effect", true);
+    public final SliderSetting gamma = addSlider("Gamma", 0.0, 20000.0, 12000.0);
+    public final BooleanSetting nightVision = addBoolean("Night Vision", true);
     public final ModeSetting mode = addMode("Mode", "Gamma", "Potion", "Both");
+    public final BooleanSetting dynamic = addBoolean("Dynamic Brightness", false);
 
-    private float oldGamma = 1.0f;
+    private float oldGamma;
 
-    public Fullbright() {
-        super("Fullbright", Category.RENDER);
-    }
-
-    @Override
-    public void onEnable() {
-        oldGamma = mc.gameSettings.gammaSetting;
-    }
+    public Fullbright() { super("Fullbright", Category.RENDER); }
 
     @Override
-    public void onDisable() {
-        mc.gameSettings.gammaSetting = oldGamma;
-    }
+    public void onEnable() { oldGamma = mc.options.getGamma().getValue(); }
+
+    @Override
+    public void onDisable() { mc.options.getGamma().setValue(oldGamma); }
 
     @Override
     public void onUpdate() {
-        if (!isToggled()) return;
+        if (!toggled) return;
         if (mode.getValue().equals("Gamma") || mode.getValue().equals("Both")) {
-            mc.gameSettings.gammaSetting = (float) gamma.getValue();
+            mc.options.getGamma().setValue(gamma.getValue());
         }
-        // Agrega efecto de poción si está activado
     }
 }

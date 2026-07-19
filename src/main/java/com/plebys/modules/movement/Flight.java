8edf786b01c22a3f50ca658c46faa1e4f8c991a1
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 15:04:42 -0700

    Refactor Flight module settings and methods

--- src/main/java/com/plebys/modules/movement/Flight.java
+++ src/main/java/com/plebys/modules/movement/Flight.java
@@ -5,26 +5,26 @@ import com.plebys.modules.Module;
 import com.plebys.settings.*;
 
 public class Flight extends Module {
-    public final ModeSetting mode = addMode("Mode", "Creative", "Vanilla", "Packet", "Smooth");
-    public final SliderSetting hSpeed = addSlider("Horizontal Speed", 0.1, 15.0, 3.0);
-    public final SliderSetting vSpeed = addSlider("Vertical Speed", 0.1, 10.0, 1.0);
-    public final BooleanSetting antiKick = addBoolean("Anti-Kick", true);
-    public final BooleanSetting autoJump = addBoolean("Auto Jump", false);
-    public final SliderSetting glide = addSlider("Glide Speed", -5.0, 0.0, -0.2);
-    public final BooleanSetting groundBoost = addBoolean("Ground Boost", true);
+    public final ModeSetting mode = addMode("Mode", "Creative", "Vanilla", "Packet");
+    public final SliderSetting speed = addSlider("Speed", 0.1, 10.0, 2.0);
+    public final SliderSetting verticalSpeed = addSlider("Vertical Speed", 0.1, 5.0, 0.8);
 
-    public Flight() { super("Flight", Category.MOVEMENT); }
+    public Flight() {
+        super("Flight", Category.MOVEMENT);
+    }
 
-    @Override public void onEnable() {}
-    @Override public void onDisable() { if (mc.player != null) mc.player.getAbilities().flying = false; }
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {
+        if (mc.player != null) mc.player.getAbilities().flying = false;
+    }
 
     @Override
     public void onUpdate() {
         if (!toggled || mc.player == null) return;
-        var abilities = mc.player.getAbilities();
-        abilities.flying = true;
-        abilities.setFlySpeed((float) hSpeed.getValue() / 10f);
-
-        // Controles verticales y lógicas avanzadas
+        mc.player.getAbilities().flying = true;
+        mc.player.getAbilities().setFlySpeed((float) (speed.getValue() / 10f));
     }
 }

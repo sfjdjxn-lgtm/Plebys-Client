a3cbf8e170c4256ed1cb8e5c95c356003b86ca41
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 15:01:32 -0700

    Add toggle method to BooleanSetting class

--- src/main/java/com/plebys/settings/BooleanSetting.java
+++ src/main/java/com/plebys/settings/BooleanSetting.java
@@ -2,10 +2,12 @@ package com.plebys.settings;
 
 public class BooleanSetting extends Setting {
     private boolean value;
+
     public BooleanSetting(String name, boolean defaultValue) {
         super(name);
         this.value = defaultValue;
     }
+
     public boolean getValue() { return value; }
     public void setValue(boolean value) { this.value = value; }
     public void toggle() { value = !value; }

d9bc8e73bc4e549a3179a6dda875796b0e921dd2
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 15:05:24 -0700

    Adjust gamma setting and improve code formatting

--- src/main/java/com/plebys/modules/movement/Fullbright.java
+++ src/main/java/com/plebys/modules/movement/Fullbright.java
@@ -5,25 +5,28 @@ import com.plebys.modules.Module;
 import com.plebys.settings.*;
 
 public class Fullbright extends Module {
-    public final SliderSetting gamma = addSlider("Gamma", 0.0, 20000.0, 12000.0);
+    public final SliderSetting gamma = addSlider("Gamma", 1.0, 20000.0, 10000.0);
     public final BooleanSetting nightVision = addBoolean("Night Vision", true);
-    public final ModeSetting mode = addMode("Mode", "Gamma", "Potion", "Both");
-    public final BooleanSetting dynamic = addBoolean("Dynamic Brightness", false);
 
     private float oldGamma;
 
-    public Fullbright() { super("Fullbright", Category.RENDER); }
+    public Fullbright() {
+        super("Fullbright", Category.RENDER);
+    }
 
     @Override
-    public void onEnable() { oldGamma = mc.options.getGamma().getValue(); }
+    public void onEnable() {
+        oldGamma = (float) mc.options.getGamma().getValue();
+    }
 
     @Override
-    public void onDisable() { mc.options.getGamma().setValue(oldGamma); }
+    public void onDisable() {
+        mc.options.getGamma().setValue(oldGamma);
+    }
 
     @Override
     public void onUpdate() {
-        if (!toggled) return;
-        if (mode.getValue().equals("Gamma") || mode.getValue().equals("Both")) {
+        if (toggled) {
             mc.options.getGamma().setValue(gamma.getValue());
         }
     }

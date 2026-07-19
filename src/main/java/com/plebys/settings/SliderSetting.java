4c397797fb25173826cb0ebaabd74ff854b5c4bd
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 14:32:43 -0700

    Add SliderSetting class for adjustable settings

---
+++ src/main/java/com/plebys/settings/SliderSetting.java
@@ -0,0 +1,15 @@
+package com.plebys.settings;
+
+public class SliderSetting extends Setting {
+    private double value, min, max;
+    public SliderSetting(String name, double min, double max, double defaultValue) {
+        super(name);
+        this.min = min;
+        this.max = max;
+        this.value = defaultValue;
+    }
+    public double getValue() { return value; }
+    public void setValue(double value) { this.value = Math.max(min, Math.min(max, value)); }
+    public double getMin() { return min; }
+    public double getMax() { return max; }
+}

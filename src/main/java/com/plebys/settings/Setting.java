503738fd1fc95b36ae85d3a3ee129e84bf26d905
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 15:00:53 -0700

    Format constructor and getter methods in Setting class

--- src/main/java/com/plebys/settings/Setting.java
+++ src/main/java/com/plebys/settings/Setting.java
@@ -2,6 +2,12 @@ package com.plebys.settings;
 
 public abstract class Setting {
     protected String name;
-    public Setting(String name) { this.name = name; }
-    public String getName() { return name; }
+
+    public Setting(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }

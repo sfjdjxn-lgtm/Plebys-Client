3afa05a0a8f9bf1c237e5f5858fbe6844afee7be
sfjdjxn-lgtm <sfjdjxn@gmail.com>
2026-07-19 15:02:23 -0700

    Refactor SliderSetting fields for immutability
    Refactor SliderSetting to use final for min and max.

--- src/main/java/com/plebys/settings/SliderSetting.java
+++ src/main/java/com/plebys/settings/SliderSetting.java
@@ -1,15 +1,21 @@
 package com.plebys.settings;
 
 public class SliderSetting extends Setting {
-    private double value, min, max;
+    private double value;
+    private final double min;
+    private final double max;
+
     public SliderSetting(String name, double min, double max, double defaultValue) {
         super(name);
         this.min = min;
         this.max = max;
         this.value = defaultValue;
     }
+
     public double getValue() { return value; }
-    public void setValue(double value) { this.value = Math.max(min, Math.min(max, value)); }
+    public void setValue(double value) { 
+        this.value = Math.max(min, Math.min(max, value)); 
+    }
     public double getMin() { return min; }
     public double getMax() { return max; }
 }
